843d87e200778dbf4e1331e5db8f82f105b94ab7
Hei <heinhtetkyaw2131@gmail.com>
2025-11-24 13:22:22 +0700

    feat: refactor TodoList and ListItem components for improved structure and functionality; add dynamic rendering of todo items fix: update class attribute to className in Header component for React compatibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css';
 
 function Header()  {
   return(
-    <header class="text-3xl flex justify-center items-center py-5 shadow-lg mb-4">
+    <header className="text-3xl flex justify-center items-center py-5 shadow-lg mb-4">
       <h1>Todo List</h1>
     </header>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,74 +1,41 @@
+import { useState } from "react";
+import ListItem from "./ListItem";
+
 function TodoList() {
+  const [todoList, setTodoList] = useState([
+    {
+      name: "Take a shower",
+      dueDate: "02/03/2023",
+      key: crypto.randomUUID(),
+    },
+    {
+      name: "Take a shower",
+      dueDate: "02/03/2023",
+      key: crypto.randomUUID(),
+    },
+    {
+      name: "Take a shower",
+      dueDate: "02/03/2023",
+      key: crypto.randomUUID(),
+    },
+    {
+      name: "Take a shower",
+      dueDate: "02/03/2023",
+      key: crypto.randomUUID(),
+    },
+    {
+      name: "Take a shower",
+      dueDate: "02/03/2023",
+      key: crypto.randomUUID(),
+    },
+  ]);
   return (
     <section className="flex flex-col mb-10 w-150 mx-auto">
       <h2 className="text-2xl">Tasks to do (3)</h2>
       <ul className="overflow-y-auto shadow-xl px-3 py-1 rounded-lg mt-5 h-88">
-        <li className="shadow-xl flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl">
-          <div>Go to the market </div> <div>02/03/2025</div>{" "}
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="shadow-xl flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl">
-          <div>Go to the market </div> <div>02/03/2025</div>
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl shadow-xl">
-          <div>Go to the market </div> <div>02/03/2025</div>
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl shadow-xl">
-          <div>Go to the market </div> <div>02/03/2025</div>
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl shadow-xl">
-          <div>Go to the market </div> <div>02/03/2025</div>
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="flex w-full justify-between items-center my-3 bg-gray-400 text-white p-5 rounded-2xl shadow-xl">
-          <div>Go to the market </div> <div>02/03/2025</div>
-          <div className="flex w-1/4 justify-between">
-            <button className="bg-blue-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Done
-            </button>
-            <button className="bg-red-500 px-2 py-1 text-white rounded-lg  shadow-xl cursor-pointer">
-              Delete
-            </button>
-          </div>
-        </li>
+        {todoList.map((item) => {
+          return <ListItem setTodoList={setTodoList} key={item.key} name={item.name} dueDate={item.dueDate} />
+        })}
       </ul>
     </section>
   );
